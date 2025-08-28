a18728e1fbea1c03b791efa6cb884e03b980de86
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-08-28 15:03:08 +0300

    Проверка фронта клиента

--- frontend/client/nuxt.config.ts
+++ frontend/client/nuxt.config.ts
@@ -1,25 +1,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: true,
+  // ssr: true,
   modules: [
     '@nuxt/image',
     '@nuxt/ui',
     '@nuxt/scripts',
     '@nuxtjs/sitemap',
   ],
-  
-  // Добавьте эти настройки
-  app: {
-    baseURL: '/', // Убедитесь, что это правильный базовый путь
-    buildAssetsDir: '_nuxt/',
-  },
-  
-  // Настройки для генерации
-  generate: {
-    fallback: '404.html'
-  },
-  
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE_URL,
