e89269048235aa79e1ba75292fcca59fe0f6c587
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-08-28 13:40:54 +0300

    Проверка бэка

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,6 +4,13 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [env('FRONTEND_URL', 'http://test.dennistp.beget.tech')],
+    // 'allowed_origins' => [
+    //     'http://localhost:3000',  // если Nuxt dev
+    //     'http://localhost:5173',  // Vite (Vue)
+    //     'http://localhost:8080',  // Vue CLI
+    //     'http://test.dennistp.beget.tech', // продакшн
+    //     'https://test.dennistp.beget.tech',
+    // ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
