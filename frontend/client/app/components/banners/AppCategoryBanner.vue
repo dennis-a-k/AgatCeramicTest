bd8533611deae3b372db7b7521bef93d2bc314b8
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-09-16 17:34:28 +0300

    Добавил баннеры категорий на главной странице

---
+++ frontend/client/app/components/banners/AppCategoryBanner.vue
@@ -0,0 +1,79 @@
+<template>
+    <section class="banner-area style-two pt-100px pb-100px">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="/">
+                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
+                            <img src="/images/page-index/banners/keramogranit.jpeg" alt="keramogranit">
+
+                            <div class="banner-content nth-child-3">
+                                <h3 class="title">Керамогранит</h3>
+                                <span class="category">
+                                    От ведущих<br>
+                                    производителей
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-md-6">
+                    <a href="/">
+                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
+                            <img src="/images/page-index/banners/plitka.jpg" alt="plitka">
+
+                            <div class="banner-content nth-child-3">
+                                <h3 class="title">Керамическая плитка</h3>
+                                <span class="category">
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-md-6">
+                    <a href="/">
+                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
+                            <img src="/images/page-index/banners/mozaika.jpeg" alt="mozaika">
+
+                            <div class="banner-content nth-child-3">
+                                <h3 class="title">Керамическая мозаика</h3>
+                                <span class="category">
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-md-6">
+                    <a href="/">
+                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
+                            <img src="/images/page-index/banners/zatirka.jpg" alt="zatirka">
+
+                            <div class="banner-content nth-child-3">
+                                <h3 class="title">Затирка для керамической плитки</h3>
+                                <span class="category">
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-md-6">
+                    <a href="/">
+                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
+                            <img src="/images/page-index/banners/santekhnika.jpeg" alt="santekhnika">
+
+                            <div class="banner-content nth-child-3">
+                                <h3 class="title">Сантехника</h3>
+                                <span class="category">
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- frontend/client/app/pages/index.vue
+++ frontend/client/app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="main-wrapper">
     <SlidersAppHeroSlider />
+    <h1 class="text-center d-none my-5">Интернет-магазин плитки, керамогранита и сантехники</h1>
+    <BannersAppCategoryBanner />
   </div>
 </template>
