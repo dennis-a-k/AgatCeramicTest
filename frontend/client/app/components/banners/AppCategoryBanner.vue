8ca43c8542a43f37005878cb07973a5a5176a01a
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-09-17 12:33:09 +0300

    Изменил компонет баннера категорий

--- frontend/client/app/components/banners/AppCategoryBanner.vue
+++ frontend/client/app/components/banners/AppCategoryBanner.vue
@@ -2,73 +2,13 @@
     <section class="banner-area style-two pt-100px pb-100px">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <a href="/">
-                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
-                            <NuxtImg src="/images/page-index/banners/keramogranit.jpeg" alt="keramogranit" />
-
-                            <div class="banner-content nth-child-3">
-                                <h3 class="title">Керамогранит</h3>
-                                <span class="category">
-                                    От ведущих<br>
-                                    производителей
-                                </span>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6">
-                    <a href="/">
-                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
-                            <NuxtImg src="/images/page-index/banners/plitka.jpg" alt="plitka" />
-
-                            <div class="banner-content nth-child-3">
-                                <h3 class="title">Керамическая плитка</h3>
-                                <span class="category">
-                                </span>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6">
-                    <a href="/">
-                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
-                            <NuxtImg src="/images/page-index/banners/mozaika.jpeg" alt="mozaika" />
-
-                            <div class="banner-content nth-child-3">
-                                <h3 class="title">Керамическая мозаика</h3>
-                                <span class="category">
-                                </span>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6">
-                    <a href="/">
-                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
-                            <NuxtImg src="/images/page-index/banners/zatirka.jpg" alt="zatirka" />
-
-                            <div class="banner-content nth-child-3">
-                                <h3 class="title">Затирка для керамической плитки</h3>
-                                <span class="category">
-                                </span>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6">
-                    <a href="/">
-                        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
-                            <NuxtImg src="/images/page-index/banners/santekhnika.jpeg" alt="santekhnika" />
-
-                            <div class="banner-content nth-child-3">
-                                <h3 class="title">Сантехника</h3>
-                                <span class="category">
-                                </span>
+                <div class="col-md-6" v-for="(banner, index) in banners" :key="index">
+                    <a :href="banner.href">
+                        <div class="single-banner mb-30px mb-lm-30px">
+                            <NuxtImg :src="banner.imgSrc" :alt="banner.imgAlt" />
+                            <div class="banner-content">
+                                <h3 class="title">{{ banner.title }}</h3>
+                                <span class="category" v-if="banner.category" v-html="banner.category"></span>
                             </div>
                         </div>
                     </a>
@@ -77,3 +17,43 @@
         </div>
     </section>
 </template>
+
+<script setup>
+const banners = [
+    {
+        href: "/keramogranit",
+        imgSrc: "/images/page-index/banners/keramogranit.jpeg",
+        imgAlt: "keramogranit",
+        title: "Керамогранит",
+        category: "От ведущих<br>производителей"
+    },
+    {
+        href: "/plitka",
+        imgSrc: "/images/page-index/banners/plitka.jpg",
+        imgAlt: "plitka",
+        title: "Керамическая плитка",
+        category: ""
+    },
+    {
+        href: "/mozaika",
+        imgSrc: "/images/page-index/banners/mozaika.jpeg",
+        imgAlt: "mozaika",
+        title: "Керамическая мозаика",
+        category: ""
+    },
+    {
+        href: "/zatirka",
+        imgSrc: "/images/page-index/banners/zatirka.jpg",
+        imgAlt: "zatirka",
+        title: "Затирка для керамической плитки",
+        category: ""
+    },
+    {
+        href: "/santekhnika",
+        imgSrc: "/images/page-index/banners/santekhnika.jpeg",
+        imgAlt: "santekhnika",
+        title: "Сантехника",
+        category: ""
+    }
+]
+</script>
