b9f855722c5b318ae343734cba95b93e46cac102
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-09-26 15:23:11 +0300

    Деплой от 26.09.2025 в3

---
+++ frontend/client/.output/public/_nuxt/BInIe9UF.js
@@ -0,0 +1,1 @@
+import{W as a,I as s,X as r,R as u,Y as o}from"#entry";function i(e){const n=e||s();return n.ssrContext?.head||n.runWithContext(()=>{if(r()){const t=u(o);if(!t)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return t}})}function d(e,n={}){const t=i(n.nuxt);return a(e,{head:t,...n})}export{d as u};
