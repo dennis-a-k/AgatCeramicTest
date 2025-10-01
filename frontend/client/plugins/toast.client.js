00d682255ccd8a9ff6e918c3e077f43ab0f3a669
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-10-01 15:41:42 +0300

    Изменил стили toast

--- frontend/client/plugins/toast.client.js
+++ frontend/client/plugins/toast.client.js
@@ -3,18 +3,50 @@ import 'vue-toastification/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const toast = createToastInterface({
+    // Позиция: 'top-right', 'top-left', 'top-center', 'bottom-right', 'bottom-left', 'bottom-center'
     position: 'top-right',
+
+    // Время показа в миллисекундах
     timeout: 3000,
+
+    // Закрывать при клике
     closeOnClick: true,
+
+    // Приостанавливать при потере фокуса
     pauseOnFocusLoss: true,
+
+    // Приостанавливать при наведении
     pauseOnHover: true,
+
+    // Перетаскивание
     draggable: true,
     draggablePercent: 0.6,
-    showCloseButtonOnHover: false,
-    hideProgressBar: false,
+
+    // Показывать кнопку закрытия при наведении
+    showCloseButtonOnHover: true,
+
+    // Скрывать прогресс-бар
+    hideProgressBar: true,
+
+    // Тип кнопки закрытия
     closeButton: 'button',
-    icon: true,
-    rtl: false
+
+    // Показывать иконки
+    icon: false,
+
+    // RTL поддержка
+    rtl: false,
+
+    // Кастомные CSS классы для разных типов
+    toastClassName: 'custom-toast',
+    bodyClassName: 'custom-toast-body',
+    progressClassName: 'custom-toast-progress',
+
+    // Максимальное количество одновременных уведомлений
+    maxToasts: 5,
+
+    // Новые уведомления появляются сверху
+    newestOnTop: true
   })
 
   return {
