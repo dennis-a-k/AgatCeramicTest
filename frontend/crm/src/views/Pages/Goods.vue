81bf11dbb4ab0d70f29c32b74a9104586c2c11f1
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-10-08 13:02:02 +0300

    Добавил мультиселект в таблицу товаров админки

--- frontend/crm/src/views/Pages/Goods.vue
+++ frontend/crm/src/views/Pages/Goods.vue
@@ -47,25 +47,58 @@
                   <input type="text" placeholder="Поиск товара"
                     class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-4 pl-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden sm:w-[300px] sm:min-w-[300px] dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
                 </div>
-                <button type="button" role="combobox" aria-controls="radix-:r4:" aria-expanded="false"
-                  aria-autocomplete="none" dir="ltr" data-state="closed" data-lov-id="src/pages/Products.tsx:145:16"
-                  data-lov-name="SelectTrigger" data-component-path="src/pages/Products.tsx" data-component-line="145"
-                  data-component-file="Products.tsx" data-component-name="SelectTrigger"
-                  data-component-content="%7B%22className%22%3A%22w-40%22%7D"
-                  class="flex h-10 items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&amp;&gt;span]:line-clamp-1 w-40"><span
-                    data-lov-id="src/pages/Products.tsx:146:18" data-lov-name="SelectValue"
-                    data-component-path="src/pages/Products.tsx" data-component-line="146"
-                    data-component-file="Products.tsx" data-component-name="SelectValue"
-                    data-component-content="%7B%22placeholder%22%3A%22%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F%22%7D"
-                    style="pointer-events: none;">Смартфоны</span><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                    stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down h-4 w-4 opacity-50"
-                    data-lov-id="src/components/ui/select.tsx:27:6" data-lov-name="ChevronDown"
-                    data-component-path="src/components/ui/select.tsx" data-component-line="27"
-                    data-component-file="select.tsx" data-component-name="ChevronDown"
-                    data-component-content="%7B%22className%22%3A%22h-4%20w-4%20opacity-50%22%7D" aria-hidden="true">
-                    <path d="m6 9 6 6 6-6"></path>
-                  </svg></button>
+
+                <div class="relative w-60" ref="multiSelectRef">
+                  <div @click="toggleDropdown"
+                    class="dark:bg-dark-900 h-11 flex items-center w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                    :class="{ 'text-gray-800 dark:text-white/90': isOpen }">
+                    <span v-if="selectedItems.length === 0" class="text-gray-400"> Категория </span>
+                    <div class="flex flex-wrap items-center flex-auto gap-2">
+                      <div v-for="item in selectedItems" :key="item.value"
+                        class="group flex items-center justify-center h-[30px] rounded-full border-[0.7px] border-transparent bg-gray-100 py-1 pl-2.5 pr-2 text-sm text-gray-800 hover:border-gray-200 dark:bg-gray-800 dark:text-white/90 dark:hover:border-gray-800">
+                        <span>{{ item.label }}</span>
+                        <button @click.stop="removeItem(item)"
+                          class="pl-2 text-gray-500 cursor-pointer group-hover:text-gray-400 dark:text-gray-400"
+                          aria-label="Remove item">
+                          <svg role="button" width="14" height="14" viewBox="0 0 14 14" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M3.40717 4.46881C3.11428 4.17591 3.11428 3.70104 3.40717 3.40815C3.70006 3.11525 4.17494 3.11525 4.46783 3.40815L6.99943 5.93975L9.53095 3.40822C9.82385 3.11533 10.2987 3.11533 10.5916 3.40822C10.8845 3.70112 10.8845 4.17599 10.5916 4.46888L8.06009 7.00041L10.5916 9.53193C10.8845 9.82482 10.8845 10.2997 10.5916 10.5926C10.2987 10.8855 9.82385 10.8855 9.53095 10.5926L6.99943 8.06107L4.46783 10.5927C4.17494 10.8856 3.70006 10.8856 3.40717 10.5927C3.11428 10.2998 3.11428 9.8249 3.40717 9.53201L5.93877 7.00041L3.40717 4.46881Z"
+                              fill="currentColor" />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <svg class="ml-auto" :class="{ 'transform rotate-180': isOpen }" width="20" height="20"
+                      viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M4.79175 7.39551L10.0001 12.6038L15.2084 7.39551" stroke="currentColor"
+                        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                  </div>
+                  <transition enter-active-class="transition duration-100 ease-out"
+                    enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+                    leave-active-class="transition duration-75 ease-in"
+                    leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+                    <div v-if="isOpen" class="absolute z-10 w-full mt-1 bg-white rounded-lg shadow-sm dark:bg-gray-900">
+                      <ul
+                        class="overflow-y-auto divide-y divide-gray-200 custom-scrollbar max-h-60 dark:divide-gray-800"
+                        role="listbox" aria-multiselectable="true">
+                        <li v-for="item in options" :key="item.value" @click="toggleItem(item)"
+                          class="relative flex items-center w-full px-3 py-2 border-transparent cursor-pointer first:rounded-t-lg last:rounded-b-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800"
+                          :class="{ 'bg-gray-50 dark:bg-white/[0.03]': isSelected(item) }" role="option"
+                          :aria-selected="isSelected(item)">
+                          <span class="grow">{{ item.label }}</span>
+                          <svg v-if="isSelected(item)" class="w-5 h-5 text-gray-400 dark:text-gray-300" fill="none"
+                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                            </path>
+                          </svg>
+                        </li>
+                      </ul>
+                    </div>
+                  </transition>
+                </div>
+
               </div>
               <div class="relative" @click="showFilter = false">
                 <button
@@ -284,7 +317,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import AdminLayout from '@/components/layout/AdminLayout.vue';
 import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue'
 import { PackageIcon, DownloadIcon, PlusIcon, Settings2Icon, SearchIcon } from "../../icons";
@@ -327,7 +360,7 @@ const fetchProducts = async () => {
     products.value = data.products.data
   } catch (err) {
     error.value = err.message
-    console.error('Error fetching products:', err)
+    console.error('Ошибка загруки товаров:', err)
   } finally {
     loading.value = false
   }
@@ -335,5 +368,68 @@ const fetchProducts = async () => {
 
 onMounted(() => {
   fetchProducts()
+})
+
+// Мультиселект
+const options = [
+  { value: 'apple', label: 'Apple' },
+  { value: 'banana', label: 'Banana' },
+  { value: 'cherry', label: 'Cherry' },
+  { value: 'date', label: 'Date' },
+  { value: 'elderberry', label: 'Elderberry' },
+  { value: 'graphs', label: 'Graphs' },
+]
+
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    default: () => [],
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const isOpen = ref(false)
+const selectedItems = ref(props.modelValue)
+const multiSelectRef = ref(null)
+
+const toggleDropdown = () => {
+  isOpen.value = !isOpen.value
+}
+
+const toggleItem = (item) => {
+  const index = selectedItems.value.findIndex((selected) => selected.value === item.value)
+  if (index === -1) {
+    selectedItems.value.push(item)
+  } else {
+    selectedItems.value.splice(index, 1)
+  }
+  emit('update:modelValue', selectedItems.value)
+}
+
+const removeItem = (item) => {
+  const index = selectedItems.value.findIndex((selected) => selected.value === item.value)
+  if (index !== -1) {
+    selectedItems.value.splice(index, 1)
+    emit('update:modelValue', selectedItems.value)
+  }
+}
+
+const isSelected = (item) => {
+  return selectedItems.value.some((selected) => selected.value === item.value)
+}
+
+const handleClickOutside = (event) => {
+  if (multiSelectRef.value && !multiSelectRef.value.contains(event.target)) {
+    isOpen.value = false
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('click', handleClickOutside)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClickOutside)
 })
 </script>
