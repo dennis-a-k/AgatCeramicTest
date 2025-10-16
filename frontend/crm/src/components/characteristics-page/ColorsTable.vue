8779fa449c1ad99ffb907bf51fa2181b9fb11cea
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-10-16 14:18:48 +0300

    Правки стилей таблицы цветов и брендов

--- frontend/crm/src/components/characteristics-page/ColorsTable.vue
+++ frontend/crm/src/components/characteristics-page/ColorsTable.vue
@@ -8,7 +8,7 @@
         </span>
         <label for="searchQuery"></label>
         <input id="searchQuery" type="text" placeholder="Поиск цвета..." v-model="searchQuery"
-          class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-12 pl-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
+          class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-10 pl-10 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
         <button v-if="searchQuery" @click="clearSearch"
           class="absolute top-1/2 right-2 -translate-y-1/2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 text-xl leading-none cursor-pointer bg-white dark:bg-gray-900 px-1 z-10">
           ✕
