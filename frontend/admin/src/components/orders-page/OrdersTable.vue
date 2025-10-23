7108c96e310b697672c1eeb2ae94c4543120050b
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-10-23 15:07:10 +0300

    Создал таблицу заказов в админке

--- frontend/admin/src/components/orders-page/OrdersTable.vue
+++ frontend/admin/src/components/orders-page/OrdersTable.vue
@@ -11,7 +11,7 @@
             Ошибка при загрузке<br />
             <button
               class="inline-flex items-center justify-center font-medium gap-2 rounded-lg transition px-4 py-3 text-sm bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300 mt-2"
-                @click="handleFetchOrders">
+              @click="handleFetchOrders">
               Попробовать снова
             </button>
           </div>
@@ -22,30 +22,30 @@
           <table v-else class="min-w-full">
             <thead>
               <tr class="border-gray-100 border-y bg-gray-50 dark:border-gray-800 dark:bg-gray-900">
-                <th class="cursor-pointer px-5 py-3 text-center w-1/11 sm:px-6">
+                <th class="cursor-pointer px-5 py-3 text-left w-2/12 sm:px-6">
                   <div class="flex items-center gap-3">
                     <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">№ Заказа</p>
                   </div>
                 </th>
-                <th class="cursor-pointer px-5 py-3 text-center w-1/11 sm:px-6">
-                  <div class="flex items-center gap-3">
+                <th class="cursor-pointer px-5 py-3 text-left w-1/12 sm:px-6">
+                  <div class="flex items-left gap-3">
                     <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Дата</p>
                   </div>
                 </th>
-                <th class="px-5 py-3 text-center w-2/11 sm:px-6">
+                <th class="px-5 py-3 text-left w-3/12 sm:px-6">
                   <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Клиент</p>
                 </th>
-                <th class="px-5 py-3 text-center w-2/11 sm:px-6">
+                <th class="px-5 py-3 text-left w-2/12 sm:px-6">
+                  <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Телефон</p>
+                </th>
+                <th class="px-5 py-3 text-left w-1/12 sm:px-6">
                   <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Товары</p>
                 </th>
-                <th class="px-5 py-3 text-center w-1/11 sm:px-6">
+                <th class="px-5 py-3 text-left w-1/12 sm:px-6">
                   <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Сумма</p>
                 </th>
-                <th class="px-5 py-3 text-center w-1/11 sm:px-6">
+                <th class="px-5 py-3 text-center w-2/12 sm:px-6" colspan="2">
                   <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Статус заказа</p>
-                </th>
-                <th class="px-5 py-3 text-center w-1/11 sm:px-6">
-                  <p class="font-bold text-gray-500 text-theme-xs dark:text-gray-400">Действие</p>
                 </th>
               </tr>
             </thead>
@@ -53,65 +53,73 @@
               <tr v-for="order in orders" :key="order.id"
                 class="border-t border-gray-100 dark:border-gray-800 hover:bg-gray-50 dark:hover:bg-gray-800">
                 <td class="px-5 py-4 sm:px-6">
-                  <p class="font-medium text-center text-gray-800 text-theme-sm dark:text-white/90">
-                    {{ order.id }}
+                  <p class="font-medium text-left text-gray-800 text-theme-sm dark:text-white/90">
+                    {{ order.order }}
                   </p>
                 </td>
                 <td class="px-5 py-4 sm:px-6">
-                  <p class="text-gray-500 text-center text-theme-sm dark:text-gray-400">
+                  <p class="text-gray-500 text-left text-theme-sm dark:text-gray-400">
                     {{ new Date(order.created_at).toLocaleDateString('ru-RU') }}
                   </p>
-                </td>
-                <td class="px-5 py-4 sm:px-6">
-                  <p class="text-gray-500 text-center text-theme-sm dark:text-gray-400">
-                    {{ order.customer_name }}
+                  <p class="text-gray-500 text-left text-theme-sm dark:text-gray-400">
+                    {{ new Date(order.created_at).toLocaleTimeString('ru-RU') }}
                   </p>
                 </td>
                 <td class="px-5 py-4 sm:px-6">
-                  <p class="text-gray-500 text-center text-theme-sm dark:text-gray-400">
+                  <span data-v-67f048e0="" class="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                    {{ order.name }}
+                  </span>
+                  <span data-v-67f048e0="" class="block text-gray-500 text-theme-xs dark:text-gray-400">
+                    {{ order.email }}
+                  </span>
+                </td>
+                <td class="px-5 py-4 sm:px-6">
+                  <p class="text-gray-500 text-left text-theme-sm dark:text-gray-400">
+                    {{ order.phone }}
+                  </p>
+                </td>
+                <td class="px-5 py-4 sm:px-6">
+                  <p class="text-gray-500 text-left text-theme-sm dark:text-gray-400">
                     {{ order.items ? order.items.length : 0 }} товаров
                   </p>
                 </td>
                 <td class="px-5 py-4 sm:px-6">
-                  <p class="font-medium text-center text-gray-800 text-theme-sm dark:text-white/90">
+                  <p class="font-medium text-left text-gray-800 text-theme-sm dark:text-white/90">
                     {{ formatter.format(order.total_amount) }}
                   </p>
                 </td>
                 <td class="px-5 py-4 sm:px-6 text-center">
                   <span v-if="order.status === 'pending'"
-                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-warning-50 text-warning-700 dark:bg-warning-500/15 dark:text-warning-500">Ожидает</span>
+                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-warning-50 text-warning-700 dark:bg-warning-500/15 dark:text-warning-500">
+                    Новый
+                  </span>
                   <span v-else-if="order.status === 'processing'"
-                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-blue-50 text-blue-700 dark:bg-blue-500/15 dark:text-blue-500">В обработке</span>
+                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-blue-50 text-blue-700 dark:bg-blue-500/15 dark:text-blue-500">
+                    В обработке
+                  </span>
                   <span v-else-if="order.status === 'shipped'"
-                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-info-50 text-info-700 dark:bg-info-500/15 dark:text-info-500">Отправлен</span>
+                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-info-50 text-info-700 dark:bg-info-500/15 dark:text-info-500">
+                    Отправлен
+                  </span>
                   <span v-else-if="order.status === 'delivered'"
-                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-success-50 text-success-700 dark:bg-success-500/15 dark:text-success-500">Доставлен</span>
+                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-success-50 text-success-700 dark:bg-success-500/15 dark:text-success-500">
+                    Доставлен
+                  </span>
                   <span v-else-if="order.status === 'cancelled'"
-                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-error-50 text-error-700 dark:bg-error-500/15 dark:text-error-500">Отменен</span>
+                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-error-50 text-error-700 dark:bg-error-500/15 dark:text-error-500">
+                    Отменён
+                  </span>
                   <span v-else
-                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-gray-100 text-gray-700 dark:bg-white/5 dark:text-white/80">{{ order.status }}</span>
+                    class="rounded-full px-2 py-0.5 text-theme-xs font-medium bg-gray-100 text-gray-700 dark:bg-white/5 dark:text-white/80">
+                    {{ order.status }}
+                  </span>
                 </td>
                 <td class="px-5 py-4 sm:px-2 text-center">
-                  <div
-                    class="inline-flex items-center justify-center gap-2 rounded-lg transition shadow-theme-xs bg-white text-gray-700 ring-1 ring-gray-300 hover:bg-success-50 hover:ring-success-300 hover:text-success-700 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-success-500/15 dark:hover:ring-success-500/50 dark:hover:text-success-500 cursor-pointer p-1 mr-3"
-                    @click="handleEdit(order)">
-                    <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20" fill="none"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M15.0911 2.78206C14.2125 1.90338 12.7878 1.90338 11.9092 2.78206L4.57524 10.116C4.26682 10.4244 4.0547 10.8158 3.96468 11.2426L3.31231 14.3352C3.25997 14.5833 3.33653 14.841 3.51583 15.0203C3.69512 15.1996 3.95286 15.2761 4.20096 15.2238L7.29355 14.5714C7.72031 14.4814 8.11172 14.2693 8.42013 13.9609L15.7541 6.62695C16.6327 5.74827 16.6327 4.32365 15.7541 3.44497L15.0911 2.78206ZM12.9698 3.84272C13.2627 3.54982 13.7376 3.54982 14.0305 3.84272L14.6934 4.50563C14.9863 4.79852 14.9863 5.2734 14.6934 5.56629L14.044 6.21573L12.3204 4.49215L12.9698 3.84272ZM11.2597 5.55281L5.6359 11.1766C5.53309 11.2794 5.46238 11.4099 5.43238 11.5522L5.01758 13.5185L6.98394 13.1037C7.1262 13.0737 7.25666 13.003 7.35947 12.9002L12.9833 7.27639L11.2597 5.55281Z"
-                        fill=""></path>
-                    </svg>
-                  </div>
-                  <div
-                    class="inline-flex items-center justify-center gap-2 rounded-lg transition shadow-theme-xs bg-white text-gray-700 ring-1 ring-gray-300 hover:bg-error-50 hover:ring-error-300 hover:text-error-700 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-error-500/15 dark:hover:ring-error-500/50 dark:hover:text-error-500 cursor-pointer p-1"
-                    @click="openDeleteModal(order)">
-                    <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20" fill="none"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M6.54142 3.7915C6.54142 2.54886 7.54878 1.5415 8.79142 1.5415H11.2081C12.4507 1.5415 13.4581 2.54886 13.4581 3.7915V4.0415H15.6252H16.666C17.0802 4.0415 17.416 4.37729 17.416 4.7915C17.416 5.20572 17.0802 5.5415 16.666 4.7915H16.3752V8.24638V13.2464V16.2082C16.3752 17.4508 15.3678 18.4582 14.1252 18.4582H5.87516C4.63252 18.4582 3.62516 17.4508 3.62516 16.2082V13.2464V8.24638V5.5415H3.3335C2.91928 5.5415 2.5835 5.20572 2.5835 4.7915C2.5835 4.37729 2.91928 4.0415 3.3335 4.0415H4.37516H6.54142V3.7915ZM14.8752 13.2464V8.24638V5.5415H13.4581H12.7081H7.29142H6.54142H5.12516V8.24638V13.2464V16.2082C5.12516 16.6224 5.46095 16.9582 5.87516 16.9582H14.1252C14.5394 16.9582 14.8752 16.6224 14.8752 16.2082V13.2464ZM8.04142 4.0415H11.9581V3.7915C11.9581 3.37729 11.6223 3.0415 11.2081 3.0415H8.79142C8.37721 3.0415 8.04142 3.37729 8.04142 3.7915V4.0415ZM8.3335 7.99984C8.74771 7.99984 9.0835 8.33562 9.0835 8.74984V13.7498C9.0835 14.1641 8.74771 14.4998 8.3335 14.4998C7.91928 14.4998 7.5835 14.1641 7.5835 13.7498V8.74984C7.5835 8.33562 7.91928 7.99984 8.3335 7.99984ZM12.4168 8.74984C12.4168 8.33562 12.081 7.99984 11.6668 7.99984C11.2526 7.99984 10.9168 8.33562 10.9168 8.74984V13.7498C10.9168 14.1641 11.2526 14.4998 11.6668 14.4998C12.081 14.4998 12.4168 14.1641 12.4168 13.7498V8.74984Z"
-                        fill=""></path>
-                    </svg>
-                  </div>
+                  <DropdownMenu :menu-items="menuItems">
+                    <template #icon>
+                      <component :is="ellipsisIcon" width="24" height="24" />
+                    </template>
+                  </DropdownMenu>
                 </td>
               </tr>
             </tbody>
@@ -123,6 +131,18 @@
 </template>
 
 <script setup>
+import DropdownMenu from '../common/DropdownMenu.vue'
+import { EllipsisIcon } from "../../icons"
+
+const ellipsisIcon = EllipsisIcon
+const menuItems = [
+  { label: 'Новый', onClick: () => console.log('Новый') },
+  { label: 'В обработке', onClick: () => console.log('В обработке') },
+  { label: 'Отправлен', onClick: () => console.log('Отправлен') },
+  { label: 'Отменён', onClick: () => console.log('Отменён') },
+  { label: 'Возврат', onClick: () => console.log('Возврат') },
+]
+
 defineProps({
   loading: Boolean,
   error: String,
@@ -130,7 +150,7 @@ defineProps({
   formatter: Object,
 })
 
-const emit = defineEmits(['fetchOrders', 'edit', 'sortBy'])
+const emit = defineEmits(['fetchOrders', 'edit'])
 
 const handleFetchOrders = () => {
   emit('fetchOrders')
