dc4bd8c0ae89e9fd809c0a4e2583e20db8613dc0
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-10-30 17:09:14 +0300

    Деплой от 30.10.2025

--- frontend/admin/dist/assets/Dashboard-DssoMN8W.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as t}from"./PageBreadcrumb.vue_vue_type_script_setup_true_lang-BkcVceme.js";import{r,c as s,o as c,w as o,a as _}from"./index-D-RsdbR5.js";const f={__name:"Dashboard",setup(l){const a=r("Статистика");return(n,i)=>(c(),s(t,null,{default:o(()=>[_(e,{pageTitle:a.value},null,8,["pageTitle"])]),_:1}))}};export{f as default};
