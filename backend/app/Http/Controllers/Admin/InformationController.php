b2a40719bb73a44cbd47fac6efab8f4879767ccd
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-10-30 15:59:09 +0300

    Реализован бэк по сохранению реквизитов компании

--- backend/app/Http/Controllers/Admin/InformationController.php
+++ backend/app/Http/Controllers/Admin/InformationController.php
@@ -27,6 +27,16 @@ class InformationController extends Controller
             'email' => 'nullable|email|max:255',
             'telegram' => 'nullable|string|max:255',
             'whatsapp' => 'nullable|string|max:255',
+            'organization' => 'nullable|string|max:255',
+            'adress' => 'nullable|string|max:255',
+            'inn' => 'nullable|string|max:255',
+            'ogrn' => 'nullable|string|max:255',
+            'okato' => 'nullable|string|max:255',
+            'okpo' => 'nullable|string|max:255',
+            'bank' => 'nullable|string|max:255',
+            'bik' => 'nullable|string|max:255',
+            'ks' => 'nullable|string|max:255',
+            'rs' => 'nullable|string|max:255',
         ]);
 
         $information = Information::first();
