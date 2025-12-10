5613d9078076002be3a85929d28a75a04e309ffe
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-12-10 15:36:43 +0300

    Изменил стили календаря

--- frontend/admin/src/components/common/MonthPicker.vue
+++ frontend/admin/src/components/common/MonthPicker.vue
@@ -18,7 +18,7 @@
         <label class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
           for="selectedYear">Год</label>
         <select id="selectedYear" v-model="selectedYear" @change="onYearChange"
-          class="w-full p-2 text-sm border border-gray-300 dark:border-gray-700 rounded bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300">
+          class="dark:bg-dark-900 h-11 flex items-center w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800">
           <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
         </select>
       </div>
