696fb1ab4c706bec15b71b93ff055739ec2841ad
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-12-23 13:50:32 +0300

    Правки в стилях поиска заказа

--- frontend/admin/src/components/orders-page/OrdersFilters.vue
+++ frontend/admin/src/components/orders-page/OrdersFilters.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="border-b border-gray-200 px-5 py-4 dark:border-gray-800">
     <div class="flex flex-col md:flex-row gap-3 sm:justify-between">
-      <div class="flex flex-col md:flex-row gap-3">
+      <div class="flex flex-col md:flex-row gap-3 w-full">
         <OrdersSearch :searchQuery="searchQuery" :searchQueryPhone="searchQueryPhone" :searchIcon="searchIcon"
-          @update:searchQuery="emit('update:searchQuery', $event)" @update:searchQueryPhone="emit('update:searchQueryPhone', $event)" />
+          @update:searchQuery="emit('update:searchQuery', $event)"
+          @update:searchQueryPhone="emit('update:searchQueryPhone', $event)" />
       </div>
       <OrdersStatusSelector :statuses="statuses" :selectedItem="selectedItem" :isOpen="isOpen"
         @toggleDropdown="emit('toggleDropdown')" @toggleItem="emit('toggleItem', $event)" />
