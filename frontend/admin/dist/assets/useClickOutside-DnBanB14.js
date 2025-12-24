baf99baee2dc60bd7c904cf293c2006ddcb695ab
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-12-24 17:05:09 +0300

    Деплой от 24.12.2025

---
+++ frontend/admin/dist/assets/useClickOutside-DnBanB14.js
@@ -0,0 +1,1 @@
+import{r as i,i as u,K as c}from"./index-IE8B9kuR.js";function s(t){const e=i(null),n=o=>{e.value&&!e.value.contains(o.target)&&t()};return u(()=>{document.addEventListener("click",n)}),c(()=>{document.removeEventListener("click",n)}),e}export{s as u};
