094ddc0d3dbee69accff78c9c1ee924059d909e2
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-12-25 14:10:31 +0300

    Удалил роуты и контроллер для цвета на стороне клиента

--- backend/routes/client/api.php
+++ backend/routes/client/api.php
@@ -2,7 +2,6 @@
 
 require __DIR__ . '/brands.php';
 require __DIR__ . '/categories.php';
-require __DIR__ . '/colors.php';
 require __DIR__ . '/products.php';
 require __DIR__ . '/checkout.php';
 require __DIR__ . '/call-request.php';
--- backend/routes/client/colors.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\Client\ColorController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('/colors', ColorController::class)->only(['index', 'show']);
